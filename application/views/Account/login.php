968744dd9551705fd464c840ac3d6cb8f5b84789
dev <dev@develop.com>
2020-05-24 15:41:31 +0530

    cahnges

--- application/views/Account/login.php
+++ application/views/Account/login.php
@@ -61,7 +61,7 @@ $this->load->view('layout/header');
                         <label>Password *</label>
                         <input type="password" name="password" placeholder="Password *" required=""/>
 
-                        <button class="btn-send-message " name="signIn" type="submit" value="signIn">Login</button>
+                        <button class="btn-send-message " name="signIn" type="button" value="signIn">Login</button>
                         <!--<span><input type="checkbox" name="remember"/>Remember Me</span>-->
                     </form>
                 </div>
@@ -143,7 +143,7 @@ $this->load->view('layout/header');
                         <br/>
 
 
-                        <button name = "registration" class="btn-send-message disabled" type="submit" value="Login">Register</button>
+                        <button name = "registration" class="btn-send-message disabled" type="button" value="Login">Register</button>
                     </form>
                 </div>
             </div>
